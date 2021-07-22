2ba358a8a1d2e5ca92088dbdb0cc701c149b2459
Umut Sever <severumut@yahoo.com>
2021-07-23 02:11:48 +0300

    Mission Detail Page UI Components Completed

---
+++ Project-SpaceX/Controller/LaunchDetail/LaunchDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LaunchDetailViewController.swift
+//  Project-SpaceX
+//
+//  Created by Umut Sever on 23.07.2021.
+//
+
+import UIKit
+
+class LaunchDetailViewController: UIViewController {
+    
+    @IBOutlet weak var missionImageView: UIImageView!
+    @IBOutlet weak var missionNameLabel: UILabel!
+    @IBOutlet weak var missionYearLabel: UILabel!
+    @IBOutlet weak var missionDetailLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+
+}
